a13e8719e26575d043980313fc463e843213fdce
Kyle Stachowicz <kylestach99@gmail.com>
2016-05-27 20:49:52 -0700

    Moved all dotfiles to to files/

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 #Make sure HOME is set
 : "${HOME:?Need to set HOME}"
 
+SEARCH_PREFIX="./files"
+
 # Create the directory structure if necessary
-for DIRECTORY in $( \
-  find . -type d -not -path "./.git*" ); do
+for DIRECTORY in $(find "$SEARCH_PREFIX" -type d); do
   mkdir -p "$HOME/$DIRECTORY"
 done
 
 # Go through all of the files that need to be used, making
 # symlinks here and overwriting previous symlinks but not files
-for FILE in $( \
-  find . -type f -not -path "./.git/*" \
-                 -not -path "./install.sh" ); do
-  if [ -f "$HOME/$FILE" ]; then
-    echo "$HOME/$FILE already exists"
-  elif [ -h "$HOME/$FILE" ]; then
-    echo "Symlink exists at $HOME/$FILE, overwriting"
-    rm "$HOME/$FILE"
-    ln -s "$(pwd)/$FILE" "$HOME/$FILE"
+for FILE in $(find "$SEARCH_PREFIX" -type f); do
+  DESTINATION="$HOME/${FILE#"$SEARCH_PREFIX/"}"
+  if [ -f "$DESTINATION" ]; then
+    echo "$DESTINATION already exists"
+  elif [ -h "$DESTINATION" ]; then
+    echo "Symlink exists at $DESTINATION, overwriting"
+    rm "$DESTINATION"
+    ln -s "$(pwd)/$FILE" "$DESTINATION"
   else
-    echo "Making file $HOME/$FILE"
-    ln -s "$(pwd)/$FILE" "$HOME/$FILE"
+    echo "Making file $DESTINATION"
+    ln -s "$(pwd)/$FILE" "$DESTINATION"
   fi
 done
 
